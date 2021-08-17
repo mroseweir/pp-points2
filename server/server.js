ac190719992a0dc6e275d60a3c9a83790baefe1d
Wesley McGhee <wesmcghee6372@gmail.com>
2021-08-17 14:28:31 -0500

    added some api commands

--- server/server.js
+++ server/server.js
@@ -9,5 +9,9 @@ app.use(cors());
 
 //commands for the controler file
 app.get("/api/points", ctrl.getPoints);
+app.get("/api/login", ctrl.getLogin);
+app.post("/api/points", ctrl.addStudent);
+app.put("/api/points/:id", ctrl.updatePoints);
+app.delete("/api/points/:id", ctrl.deletePoints);
 
 app.listen(port, () => console.log(`Sever is up and running on ${port}...`));
