c97d0abb1d4e829bfb5a49ed89e6f942776e0620
Logan Bready <logan.bready@gmail.com>
2021-08-18 10:30:07 -0600

    added basic layout to the add student page

--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Header from "../components/Header";
+
 export default function AddStudent() {
   return (
     <div>
       <Header />
-      <h1>Add Students</h1>
+      <div className="main-page">
+        <div className="student-entry">
+          <h2>Student Entry</h2>
+        </div>
+        <form>
+          <label for="student" placeholder="Student"></label>
+          <input type="text" name="student" placeholder="Student"></input>
+          <label for="cohort" placeholder="Cohort"></label>
+          <input type="text" name="cohort" placeholder="Cohort"></input>
+          <label for="specialization" placeholder="specialization"></label>
+          <input
+            type="text"
+            name="specialization"
+            placeholder="specialization"
+          ></input>
+          <input type="submit" name="add" placeholder="Submit"></input>
+        </form>
+      </div>
     </div>
   );
 }
