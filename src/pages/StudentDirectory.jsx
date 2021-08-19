986a2a16ba0d6071a9b14f05cf2ddad68655069c
Wesley McGhee <wesmcghee6372@gmail.com>
2021-08-18 20:01:48 -0500

    axios

--- src/pages/StudentDirectory.jsx
+++ src/pages/StudentDirectory.jsx
@@ -1,69 +1,23 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
-
 const axios = require("axios");
 
-let students = require("../dataTest.json");
-// console.log(students);
-
-const getStudents = () => {
-  axios.get("http://localhost:4500/api/points").then((res) => {
-    console.log(res.data);
-    return res.data;
-  });
-};
-
-getStudents();
-
-const StudentList = (props) => {
-  return (
-    <div>
-      {students.map((profile) => (
-        <Card {...profile} />
-      ))}
-    </div>
-  );
-};
-
-class Card extends Component {
-  render() {
-    const student = this.props;
-    // console.log(student);
-    return (
-      <div
-        className="student-info"
-        style={{
-          backgroundColor: "#0177a9",
-          color: "#fff",
-          display: "flex",
-          justifyContent: "center",
-          margin: "15px",
-        }}
-      >
-        <div>Name: {student.name} </div>
-        <div>Cohort: {student.cohort} </div>
-        <div>Points: {student.points}</div>
-      </div>
-    );
-  }
-}
-
-class StudentApp extends Component {
-  render() {
-    return (
-      <div>
-        <StudentList />
-      </div>
-    );
-  }
-}
+const baseURL = `http://localhost:4500/api/points`;
 
 export default function StudentDirectory() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((res) => {
+      setPost(res.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
     <div>
-      <Header />
-      <h1>Student Directory</h1>
-      <StudentApp />
+      <h1>{post[0].name}</h1>
     </div>
   );
 }
