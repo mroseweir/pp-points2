32150d41c9ba9974c0a8a8654b1ecea987c2b9c4
mroseweir <roseweirmichael@gmail.com>
2021-08-20 15:43:54 -0500

    adds functionality to search feature

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,17 +3,17 @@ import React from "react";
 function Footer() {
   const footerArr = [
     '"Pair Programming is better than not Pair Programming" -somebody, probably',
-    '"Two heads are better than one"-IDK',
-    '"Keep calm and pair program"-Elon Musk',
-    '"Oh your one of those people ローロ"-You know who',
-    '"Do people actually read these"-Minecraft',
-    '"Only kings Pair Program"-TheIronicAnimeGirl',
-    '"He\'s the best gamer I ever saw"-Technoblade',
-    '"Make Pair Programing Great Again"-Tonald Drump',
-    '"I walk a lonely road, unless I am pair programming"-Blue Day',
-    '"I\'m running out of ideas"-The guy making these',
-    '"I wOrk BeTtEr bY mY sElF"-Ineffeciency',
-    '"Got a bug in your code FLEX TAPE"-Knock off Billy Mays',
+    '"Two heads are better than one" -IDK',
+    '"Keep calm and pair program" -Elon Musk',
+    '"Oh, your one of those people ローロ" -You know who',
+    '"Do people actually read these" -Minecraft',
+    '"Only kings Pair Program" -TheIronicAnimeGirl',
+    '"He\'s the best gamer I ever saw" -Technoblade',
+    '"Make Pair Programing Great Again" -Tonald Drump',
+    '"I walk a lonely road, unless I am pair programming" -Blue Day',
+    '"I\'m running out of ideas" -The guy making these',
+    '"I wOrk BeTtEr bY mY sElF" -Ineffeciency',
+    '"Got a bug in your code FLEX TAPE" -Knock off Billy Mays',
   ];
   function randoIndex() {
     return Math.floor(Math.random() * footerArr.length);
---
+++ src/pages/StudentDirectoryAdmin 2.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+const axios = require("axios");
+
+const baseURL = `http://localhost:4500/api/points`;
+
+export default function StudentDirectory() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((res) => {
+      setPost(res.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+    <div>
+      <Header />
+      <h1>Student Directory</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <p>{post[0].name}</p>
+        <p>{post[0].cohort}</p>
+        <p>{post[0].points}</p>
+      </div>
+    </div>
+  );
+}
