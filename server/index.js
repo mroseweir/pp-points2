42b53ce17e5d7f3a9a054718cb543e12361ae32e
mroseweir <roseweirmichael@gmail.com>
2021-08-25 22:01:49 -0500

    adding procfile to test deployment

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 
 app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  response.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 //commands for the controler file
